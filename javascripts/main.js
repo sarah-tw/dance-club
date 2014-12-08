431e1de0de39db01c1f3efa332dc16b4059e4e2c
Sarah <sixiaojing1988@gmail.com>
2014-12-08 18:17:32 +0800

    made dance club page responsive.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,41 @@
 $(document).ready(function(){
-	 $('#live-dance-show').slidesjs({
-    width: 1000,
-    height: 666,
-    play: {
-    	auto: true,
-    	pauseOnHover: true
-    }
-  });
+	$('#live-dance-show').slidesjs({
+        width: 1000,
+        height: 666,
+        play: {
+        	auto: true,
+        	pauseOnHover: true
+        }
+    });
 
-	 $('#new-year-slide').slidesjs({
-    width: 500,
-    height: 333,
-    play: {
-    	auto: false,
-    	pauseOnHover: true
-    },
-    pagination: {
-    	active: false
-    }
-  });
+	$('#new-year-slide').slidesjs({
+        width: 500,
+        height: 333,
+        play: {
+        	auto: false,
+        	pauseOnHover: true
+        },
+        pagination: {
+        	active: true
+        },
+        navigation: {
+            active: true
+        }
+    });
+    resetDanceSlide();
+    resetNewYearSlide();
 
-	var danceSlideWidth = $('#live-dance-show').width();
-	$('#live-dance-show, #live-dance-show .slidesjs-control, #live-dance-show, .slidesjs-container, #live-dance-show .slide-item').height(danceSlideWidth*0.6);
-	var newYearSlideWidth = $('#new-year-slide').width();
-	$('#new-year-slide, #new-year-slide .slidesjs-control, #new-year-slide .slidesjs-container, #new-year-slide .slide-item').height(newYearSlideWidth*0.8);
+    $(window).resize(function(){
+        resetDanceSlide();
+        resetNewYearSlide();
+    });
+
+    function resetDanceSlide(){
+        var danceSlideWidth = $('#live-dance-show').width();
+        $('#live-dance-show, #live-dance-show .slidesjs-control, #live-dance-show, .slidesjs-container, #live-dance-show .slide-item').height(danceSlideWidth*0.6);    
+    }
+    function resetNewYearSlide(){
+        var newYearSlideWidth = $('#new-year-slide').width();
+        $('#new-year-slide, #new-year-slide .slidesjs-control, #new-year-slide .slidesjs-container, #new-year-slide .slide-item').height(newYearSlideWidth*0.8);    
+    }
 });
